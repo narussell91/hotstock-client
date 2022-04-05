ee0a1a4d205259cdc2a6be09698976618b737fde
nate <nate@Nates-Air.lan>
2022-04-05 00:24:46 -0400

    add a dummy page

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import Vuex from "vuex";
 import App from "./App.vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import router from "./router.js";
@@ -8,6 +9,7 @@ import "bootswatch/dist/lux/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
+Vue.use(Vuex);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.use(HighchartsVue);
--- src/router.js
+++ src/router.js
@@ -5,13 +5,15 @@ import ContactUs from "../src/views/ContactUs.vue";
 import AnalyticsDashboard from "../src/views/AnalyticsDashboard.vue";
 import SignIn from "../src/views/SignIn.vue";
 import DataPrivacy from "../src/views/DataPrivacy.vue";
+import DummyPage from "../src/views/DummyPage.vue";
 
 Vue.use(Router);
 
 const router = new Router({
   mode: "history",
   routes: [
-    { path: "/", name: "signin", component: SignIn },
+    { path: "/", name: "dummy", component: DummyPage },
+    { path: "/signin", name: "signin", component: SignIn },
     { path: "/dashboard", name: "dashboard", component: AnalyticsDashboard },
     { path: "/contactus", name: "contactUs", component: ContactUs },
     { path: "/privacy", name: "privacy", component: DataPrivacy },
