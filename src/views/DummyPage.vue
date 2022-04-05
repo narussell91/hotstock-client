6d51a46fee7704c5da07a52386f7c5e29666d6c6
nate <nate@Nates-Air.lan>
2022-04-05 01:02:33 -0400

    add alert after signin

---
+++ src/views/DummyPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dummy-box">
+    <b-col>
+      <h1>THIS IS A DUMMY PAGE.</h1>
+      <h1>CLICK ENTER TO CONTINUE TO SIGN IN</h1>
+      <b-button class="btn float-right" variant="dark" to="/signin"
+        >ENTER</b-button
+      >
+    </b-col>
+  </div>
+</template>
+<style>
+.dummy-box {
+  margin: 10%;
+}
+</style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -39,7 +39,11 @@
           </b-row>
           <b-row>
             <b-col>
-              <b-button class="btn float-right" type="submit" variant="dark"
+              <b-button
+                v-on:click="greeting"
+                class="btn float-right"
+                type="submit"
+                variant="dark"
                 >Submit</b-button
               >
             </b-col>
@@ -50,6 +54,7 @@
   </div>
 </template>
 <script>
+import router from "@/router";
 export default {
   data() {
     return {
@@ -61,6 +66,14 @@ export default {
   computed: {
     validation() {
       return this.userId.length > 4 && this.userId.length < 13;
+    },
+  },
+  methods: {
+    greeting: function () {
+      alert(
+        "Stock recommendations and comments presented on hotstock.com are solely those of the analysts and experts quoted. They do not represent the opinions of hotstock.com on whether to buy, sell or hold shares of a particular stock. Investors should be cautious about any and all stock recommendations and should consider the source of any advice on stock selection. Various factors, including personal or corporate ownership, may influence or factor into an expert's stock analysis or opinion. All investors are advised to conduct their own independent research into individual stocks before making a purchase decision. In addition, investors are advised that past stock performance is no guarantee of future price appreciation. By clicking OK you are  verifiying that you are over the age of 18"
+      );
+      router.push("/dashboard");
     },
   },
 };
